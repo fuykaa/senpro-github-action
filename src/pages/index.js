debf9574a9ba94c02c6c11722286d52369a2d341
fuykaa <fuykaumrsnkdw6@gmail.com>
2026-05-20 10:59:59 +0700

    Mengubah Nama NIM dan sengaja menggagalkan build Case 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 //import Image from 'next/image'
 //import { Inter } from '@next/font/google'
 //import styles from '@/styles/Home.module.css'
-
+// anu
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
