11d04d28cd4e41a2f99aa494dfe46554459e750b
fuykaa <fuykaumrsnkdw6@gmail.com>
2026-05-20 09:50:02 +0700

    yaml step 2 deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            [Kistosi Al Ghifari] | [515523]
           </h5>
         </div>
       </main>
